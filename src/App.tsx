e5c3ac665ef3062d4d9ac0ee15a853cafce04ba8
zym <202213093023@cuc.edu.cn>
2025-05-14 21:46:45 +0800

    fix a bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import OptionComparator from './components/OptionComparator';
 import IfOnlySimulator from './components/IfOnlySimulator';
